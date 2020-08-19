155060b01d5a20a357ded7d294d00212f2ca2cc2
yan <i@yinyan.fr>
2020-08-19 14:49:56 +0200

    shipping rosalyn webclient

---
+++ client/precache-manifest.2cac3aa8c338e45ee8037cee129179ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694abe436e44c3493927b0ec34c83851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "060fadc7f65163a6cf4c",
+    "url": "/static/css/2.2c3eb1a5.chunk.css"
+  },
+  {
+    "revision": "060454899b3e1cafaeba",
+    "url": "/static/css/main.3bcb3123.chunk.css"
+  },
+  {
+    "revision": "060fadc7f65163a6cf4c",
+    "url": "/static/js/2.b533d1c2.chunk.js"
+  },
+  {
+    "revision": "2bbcbfb9846bb6dd379fdefc5ae3898a",
+    "url": "/static/js/2.b533d1c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060454899b3e1cafaeba",
+    "url": "/static/js/main.ddbadc82.chunk.js"
+  },
+  {
+    "revision": "295fb532bc88107e8424",
+    "url": "/static/js/runtime-main.e9d8f86d.js"
+  },
+  {
+    "revision": "ae6ba5e3d72dc94e3c8345b7f42b2de1",
+    "url": "/static/media/note1024.ae6ba5e3.png"
+  }
+]);
--- src/app.js
+++ src/app.js
@@ -22,12 +22,12 @@ app.use("/library", library)
 app.use("/playlist", playlist)
 
 app.use(express.static(
-    path.join(__dirname, "..", "build")
+    path.join(__dirname, "..", "client")
 ))
 
 app.get("*", (req, res) => {
     res.sendFile(
-        path.join(__dirname, "..", "build", "index.html")
+        path.join(__dirname, "..", "client", "index.html")
     )
 })
 
